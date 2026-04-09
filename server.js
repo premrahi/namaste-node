489d7d03e6be4a3fa82f253b7b8f65b4c75edf84
premrahi <premrahi234@gmail.com>
2026-04-10 01:41:50 +0530

    created first server

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const http = require("http") ;
 
 
 const server = http.createServer((req,res) => {
+
+    if(req.url === "/getSecretData"){
+        res.end(" there is no secret data ");
+    }
     res.end("hello fellas!, Gulab jaamun this side") ;
 }) ;
 
